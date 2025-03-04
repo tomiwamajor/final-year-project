bd84f41dc27f0ada5bfb36b4c21013299cf61c9b
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2025-03-04 08:03:47 +0000

    ft: checkout page font size reduced

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -2,6 +2,6 @@
 import { AbsoluteCenter, Text } from "@chakra-ui/react";
 export default function CheckoutPage() {
     return (
-        <AbsoluteCenter fontSize={'30'}>This page is currently under construction</AbsoluteCenter>
+        <AbsoluteCenter fontSize={'20'}>This page is currently under construction</AbsoluteCenter>
     )
 }
